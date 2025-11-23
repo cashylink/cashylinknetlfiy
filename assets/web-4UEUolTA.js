c708b7320b4b57ca726126a31d110c6bb2d834d8
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-11-23 22:54:02 +0200

    update code

--- assets/web-4UEUolTA.js
+++
@@ -1,1 +0,0 @@
-import{a5 as n}from"./index-CMr4Oi63.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
