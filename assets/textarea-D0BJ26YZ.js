6cf460283e54054dc9cfdfb23aa32b414b16dc27
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-11-24 00:00:03 +0200

    update code

--- assets/textarea-D0BJ26YZ.js
+++
@@ -1,6 +0,0 @@
-import{O as o,r as a,j as t,W as i}from"./index-B57-Wohm.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const l=o("Bell",[["path",{d:"M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9",key:"1qo2s2"}],["path",{d:"M10.3 21a1.94 1.94 0 0 0 3.4 0",key:"qgo35s"}]]),n=a.forwardRef(({className:e,...r},s)=>t.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:s,...r}));n.displayName="Textarea";export{l as B,n as T};
---
+++ assets/web-Don691vE.js
@@ -0,0 +1,1 @@
+import{ab as n}from"./index-5OsgE1a1.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
