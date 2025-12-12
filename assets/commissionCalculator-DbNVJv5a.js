b8e703dc9c8bbe7bf0c89d28a61d023c74221b5d
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-12 16:17:54 +0200

    update code

--- assets/commissionCalculator-DbNVJv5a.js
+++
@@ -1,2 +0,0 @@
-function e(t,n){return n==="send"?0:Math.round(t*.01*100)/100}function i(t,n,o=0){const r=e(t,n),c=Math.max(0,r-o);return Math.round(c*100)/100}export{e as calculateCommission,i as calculateProfit};
-//# sourceMappingURL=commissionCalculator-DbNVJv5a.js.map
--- assets/web-l5uzc54L.js
+++
@@ -1,2 +0,0 @@
-import{ab as n}from"./index-DKHjTn06.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-l5uzc54L.js.map
