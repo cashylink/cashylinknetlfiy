e84d44d72603e3d1624aaef467634e845837e21b
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-14 20:23:49 +0200

    update code

--- assets/web-CwYSDaHQ-TFogmDaD.js
+++
@@ -1,2 +0,0 @@
-import{D as r}from"./index-C_crvORM.js";class o extends r{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-CwYSDaHQ-TFogmDaD.js.map
