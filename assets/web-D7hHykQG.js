9d3e0efe200f503b0117f85cd5e0b37cbac194d3
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-14 21:31:44 +0200

    update code

---
+++ assets/web-D7hHykQG.js
@@ -0,0 +1,2 @@
+import{aa as n}from"./index-sOXjUJgE.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-D7hHykQG.js.map
