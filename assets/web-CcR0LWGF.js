17785bb4ac0329b0160d56755f5585fd81f923d9
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-14 20:47:56 +0200

    update code

---
+++ assets/web-CcR0LWGF.js
@@ -0,0 +1,2 @@
+import{aa as n}from"./index-D460zBFD.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-CcR0LWGF.js.map
--- assets/web-DTz8jim7.js
+++
@@ -1,2 +0,0 @@
-import{aa as n}from"./index-B_ABn6sw.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-DTz8jim7.js.map
