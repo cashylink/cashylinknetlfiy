fc2805b2c57c921f61ddf12853b24baf8834837c
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-21 22:20:45 +0200

    update code

---
+++ assets/web-rxg5hbaV.js
@@ -0,0 +1,2 @@
+import{ac as n}from"./index-B6qHa3zL.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-rxg5hbaV.js.map
