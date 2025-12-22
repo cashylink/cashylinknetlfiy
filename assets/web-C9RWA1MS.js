60a0476710c1c73ba9ec6417d089dccdcd06d9bb
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-22 15:26:10 +0200

    update code

--- assets/web-C9RWA1MS.js
+++
@@ -1,2 +0,0 @@
-import{ac as n}from"./index-CpSuNyR6.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-C9RWA1MS.js.map
---
+++ assets/web-D3F13_KI.js
@@ -0,0 +1,2 @@
+import{aa as n}from"./index-DA5EGBVC.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-D3F13_KI.js.map
