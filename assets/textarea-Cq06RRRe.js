16f6733e34678485566917c965de07415d6a3354
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-25 03:47:18 +0200

    update code

--- assets/textarea-Cq06RRRe.js
+++
@@ -1,7 +0,0 @@
-import{H as a,r as s,j as t,Q as i}from"./index-DA5EGBVC.js";/**
- * @license lucide-react v0.561.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=[["path",{d:"M10.268 21a2 2 0 0 0 3.464 0",key:"vwvbt9"}],["path",{d:"M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326",key:"11g9vi"}]],l=a("bell",n),d=s.forwardRef(({className:e,...o},r)=>t.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:r,...o}));d.displayName="Textarea";export{l as B,d as T};
-//# sourceMappingURL=textarea-Cq06RRRe.js.map
---
+++ assets/web-BX8U7p99.js
@@ -0,0 +1,2 @@
+import{a8 as n}from"./index-DvnSb1Lh.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-BX8U7p99.js.map
