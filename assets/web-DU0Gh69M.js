7059c1581f9397ec40b6424a05e5ef6791c1b7b9
alkaptin2030-jpg <alkaptin2030@gmail.com>
2025-12-31 21:08:51 +0200

    update code

--- assets/web-DU0Gh69M.js
+++
@@ -1,2 +0,0 @@
-import{ac as n}from"./index-Cty74lt-.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-DU0Gh69M.js.map
---
+++ assets/web-WULI_Ezz.js
@@ -0,0 +1,2 @@
+import{ac as n}from"./index-DCCNgGPW.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-WULI_Ezz.js.map
