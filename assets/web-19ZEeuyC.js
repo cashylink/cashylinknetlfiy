2706fc0e909e9ae08f93444bba85156b1c6f22d8
alkaptin2030-jpg <alkaptin2030@gmail.com>
2026-01-01 17:29:59 +0200

    update code

--- assets/web-19ZEeuyC.js
+++
@@ -1,2 +0,0 @@
-import{ac as n}from"./index-DCCNgGPW.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
-//# sourceMappingURL=web-19ZEeuyC.js.map
---
+++ assets/web-Dv7ckV1-.js
@@ -0,0 +1,2 @@
+import{av as n}from"./index-CWTdLx7E.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-Dv7ckV1-.js.map
