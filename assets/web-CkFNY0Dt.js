9ae8d9ccd9f6e339fbc66bb7981b3aa133a54523
cashylink <alkaptin2030@gmail.com>
2026-02-02 00:17:00 +0200

    update code

---
+++ assets/web-CkFNY0Dt.js
@@ -0,0 +1,2 @@
+import{aC as n}from"./index-CZwBW8vR.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-CkFNY0Dt.js.map
---
+++ assets/web-CpaF4ZFc.js
@@ -0,0 +1,2 @@
+import{aC as n}from"./index-CZwBW8vR.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
+//# sourceMappingURL=web-CpaF4ZFc.js.map
