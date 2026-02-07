f4308f91fc067df27ff799620df744a4c0a4de17
cashylink <alkaptin2030@gmail.com>
2026-02-07 02:31:54 +0200

    update code

---
+++ assets/web-B90a_bF0.js
@@ -0,0 +1,2 @@
+import{aE as n}from"./index-BhZSAJfd.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-B90a_bF0.js.map
--- assets/web-BElVOeF0.js
+++
@@ -1,2 +0,0 @@
-import{aC as n}from"./index-B_gBGKq7.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
-//# sourceMappingURL=web-BElVOeF0.js.map
